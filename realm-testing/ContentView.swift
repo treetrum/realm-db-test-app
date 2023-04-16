893bf3e405c7f6b948bc76ad6da63b0c714dbc4c
Sam Davis <sam@sjd.co>
2023-04-16 10:48:25 +1000

    Initial Commit

---
+++ realm-testing/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  realm-testing
+//
+//  Created by Samuel Davis on 16/4/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ realm-testing/realm_testingApp.swift
@@ -0,0 +1,17 @@
+//
+//  realm_testingApp.swift
+//  realm-testing
+//
+//  Created by Samuel Davis on 16/4/2023.
+//
+
+import SwiftUI
+
+@main
+struct realm_testingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
